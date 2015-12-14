a29f456be0119a1e0f0e28ab99e2d65255712660
Adam Doan <doana@uoguelph.ca>
2015-12-14 10:31:25 -0500

    A little more specifics around geolocation, some better input handling around query building.

--- ttm.py
+++ ttm.py
@@ -7,14 +7,35 @@ auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
 auth.set_access_token(secrets.access_token_key, secrets.access_token_secret)
 api = tweepy.API(auth)
 
-search_term = "Android"
+search_term = "BecauseIts2015"
+location_term = None
 
-place = getPlaceID(api=api, place="Canada")
-query = "place:{place}".format(place=place)
+query = ""
+if(location_term):
+    place = getPlaceID(api=api, place=location_term)
+    if(place):
+        query += "place:{place}".format(place=place)
+
 if search_term:
     query += " " + search_term
+
 tweets = api.search(q=query, count=100)
 
-print len(tweets)
-#for tweet in tweets:
-#x`    pprint(tweet)
+count = len(tweets)
+print "{count} Results found".format(count=count)
+
+for tweet in tweets:
+    #pprint(tweet)
+    print "Place: "
+    pprint(tweet.place)
+    place = tweet.place
+
+    if(place):
+        pprint(place.full_name)
+        pprint(place.country)
+
+    print "Geo: "
+    pprint(tweet.geo)
+    print "Coordinates"
+    pprint(tweet.coordinates)
+
